665ccced87ddd468da81d2aefc7653f57ebf7770
Saumya Garg <garg.saumya20@gmail.com>
2021-04-05 01:21:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Basket from "./Components/Basket";
+import data from "./data";
+import { useState } from "react";
+import "./index.css";
+function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+  return (
+    <div className="App">
+      <Header countCartItems={cartItems.length}></Header>
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Basket>
+      </div>
+    </div>
+  );
+}
+
+export default App;
